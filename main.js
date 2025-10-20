50f1a47d92c476482f1f3c1129ae884306c3adfd
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2025-10-19 21:37:52 -0400

    Deleted a file, got my ids, cleaned up css and breaking down things by bits

---
+++ main.js
@@ -0,0 +1,9 @@
+ const color = document.getElementById('color')
+ const colorBtn = document.getElementById('color-btn')
+ const colorBars = document.getElementById('color-pallette')
+ const selectedColor = document.getElementById('select-color')
+ const colorForm = document.getElementById('color-form')
+ 
+/*
+
+*/
